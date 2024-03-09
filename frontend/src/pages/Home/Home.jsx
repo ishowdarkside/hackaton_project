a8da7be93c7bd5bb297cbfc75ae28b8e879c1842
visiot-dev <105046927+visiot-dev@users.noreply.github.com>
2024-03-09 21:43:07 +0100

    fix

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,20 +1,10 @@
-// SCSS
 import styles from "./Home.module.scss";
-import Signup from "../../components/Signup/Signup";
 import ChooseLevel from "../../components/ChooseLevel/ChooseLevel";
-import AccountCreated from "../../components/AccountCreated/AccountCreated";
 import { useOnboardingContext } from "../../context/OnboardingContext";
 import ChooseStudy from "../../components/Plan/ChooseStudy";
-//images
 import logo from "../../assets/logo/logo.png";
 import excitedLogo from "../../assets/logo/Excited.png";
-import angryLogo from "../../assets/logo/Angry.png";
-import choiceLogo from "../../assets/logo/Choice.png";
-import confusedLogo from "../../assets/logo/Confused.png";
-import dangerLogo from "../../assets/logo/Danger.png";
-import ideaLogo from "../../assets/logo/Idea.png";
 import impressedLogo from "../../assets/logo/Impressed.png";
-import searchingLogo from "../../assets/logo/Searching.png";
 import SplashScreen from "../../components/SplashScreen/SplashScreen";
 
 export default function Home() {
